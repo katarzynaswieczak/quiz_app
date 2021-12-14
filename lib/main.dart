c9df3a40bbcef1eb43d71a45eb9c90d78da214ff
katarzynaswieczak <katarzynaswieczak.97@gmail.com>
2021-12-14 17:06:08 +0100

    add ternary expression

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,60 @@ class Quiz_App extends StatefulWidget {
 }
 
 class Quiz_AppState extends State<Quiz_App> {
-  var questionIndex = 0;
+  final questions = const [
+    {
+      'questionText': 'What\s your favourite color?',
+      'answers': ['Black', 'Blue', 'Pink', 'Yellow'],
+    }, //map
+    {
+      'questionText': 'What\s your favourite animal?',
+      'answers': ['Elephant', 'Cat', 'Rabbit', 'Dog'],
+    },
+    {
+      'questionText': 'Who\s your favourite instructor?',
+      'answers': ['Alex', 'Max', 'Kate', 'Sam'],
+    },
+  ];
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
 
-    print(questionIndex);
+    print(_questionIndex);
+
+    //I'm  checking if question index is smaller than question length
+    if (_questionIndex < questions.length) {
+      print('I have more questions!');
+    } else {
+      print('No more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\s your favourite color?',
-        'answers': ['Black', 'Blue', 'Pink', 'Yellow'],
-      }, //map
-      {
-        'questionText': 'What\s your favourite animal?',
-        'answers': ['Elephant', 'Cat', 'Rabbit', 'Dog'],
-      },
-      {
-        'questionText': 'Who\s your favourite instructor?',
-        'answers': ['Alex', 'Max', 'Kate', 'Sam'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[questionIndex]['questionText'] as String,
-              //on all the questions, I'm accessing the question with index(0,1,2..)
-              //and then for the chosen question map, I access the value with the key questionText
-            ),
-            ...(questions[questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(answerQuestion, answer);
-              //transform every answer into an answer widget ...
-            }).toList() //I make sure that this is a list and then I take these generated answer widgets ...
-          ], // and I add it to this list
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'] as String,
+                    //on all the questions, I'm accessing the question with index(0,1,2..)
+                    //and then for the chosen question map, I access the value with the key questionText
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                    //transform every answer into an answer widget ...
+                  }).toList() //I make sure that this is a list and then I take these generated answer widgets ...
+                ], // and I add it to this list
+              )
+            : Center(child: Text('You did it!')),
       ),
     );
   }
